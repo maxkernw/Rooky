d572bf9a82f09e308b06ec3409fbb8a0db0681da
Max Tötterman <max.philip.totterman@gmail.com>
2015-12-11 14:05:57 +0100

    More reasonable http status codes

--- API/v1/handlers/person.js
+++ API/v1/handlers/person.js
@@ -3,16 +3,18 @@ var person = {}
 var controller = require('../controllers/personController')
 
 person.create = function(req, res) {
-    controller.create(function(err, item) {
+    controller.create(
+      function(err, item) {
           if (err) return res.status(400).send(err);
 
-          res.status(200).send('Created' + item);
+          res.status(200).location('v1/people/' + item._id).send('Created' + item);
       },
       req.body)
 }
 
 person.findAll = function(req, res) {
-    controller.findAll(function(err, item) {
+    controller.findAll(
+      function(err, item) {
           if (err) res.status(400).send(err)
 
           res.status(200).send(item)
@@ -21,7 +23,8 @@ person.findAll = function(req, res) {
 }
 
 person.findById = function(req, res) {
-    controller.findById(function(err, item) {
+    controller.findById(
+      function(err, item) {
           if (err) res.status(400).send(err)
 
           res.status(200).send(item)
@@ -33,6 +36,7 @@ person.update = function(req, res) {
     controller.update(
       function(err, doc) {
           if (err) return res.status(400).send(err)
+
           res.status(200).send(doc)
       },
       req.body,
@@ -40,9 +44,11 @@ person.update = function(req, res) {
 }
 
 person.delete = function(req, res) {
-    controller.delete(function(err, doc) {
+    controller.delete(
+      function(err, doc) {
           if (err) return res.status(400).send('Error ' + err)
-          return res.status(200).send('Deleted');
+
+          res.status(204).send();
       },
       req.params.id)
 }
