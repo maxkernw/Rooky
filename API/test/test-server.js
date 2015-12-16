0d9807da97d988fbfff2440975a4b26bc874c8a0
Max Tötterman <max.philip.totterman@gmail.com>
2015-12-16 14:38:27 +0100

    Changed test desc..

--- API/test/test-server.js
+++ API/test/test-server.js
@@ -6,7 +6,7 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Blobs', function() {
-    it('should get a pong response', function(done) {
+    it('should get a 200 response', function(done) {
         chai.request(server)
         .get('/ping')
         .end(function(err, res) {
