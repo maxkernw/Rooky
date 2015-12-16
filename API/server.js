7f4568f2f065a8be720cf0b2336da7db4f77958b
Max Tötterman <max.philip.totterman@gmail.com>
2015-12-16 15:03:38 +0100

    Config files added

--- API/server.js
+++ API/server.js
@@ -3,11 +3,14 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 var express = require('express')
 var mongoose = require('mongoose')
-var v1 = require('./v1/')
+var jwt = require('jsonwebtoken');
+var config = require('./config');
 
+var v1 = require('./v1/')
 var app = express()
 
-mongoose.connect('mongodb://localhost/simple')
+app.set('superSecret', config.secret);
+mongoose.connect(config.database)
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(cors())
 app.use(v1)
